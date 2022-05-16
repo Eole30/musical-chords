cc1df26ee7c2b5a089ed4d8f8952c296b36af720
Elisabeth <e.fauquet3@gmail.com>
2022-05-16 20:22:56 +0200

    switching between libs according to the os used

---
+++ app/python/main.py
@@ -0,0 +1,15 @@
+import ctypes as ct
+import sys
+
+lib_path_linux = "../../lib/library.so"
+lib_path_windows = "../../lib/library.dll"
+
+if(sys.platform == "linux"):
+    _lib = ct.cdll.LoadLibrary(lib_path_linux)
+if(sys.platform == "win32"):
+    _lib = ct.cdll.LoadLibrary(lib_path_windows)
+
+##testing if the lib works
+print(_lib)
+_lib.hello()
+print(_lib.returns_int())
