5ffe360500b8f5dd7b8e93fe8b180c26997ee823
Eole30 <Chep.j15@gmail.com>
2022-07-14 18:48:54 +0200

    rosenblatt start

--- lib/linear_model.c
+++ lib/linear_model.c
@@ -12,7 +12,7 @@ double get_random_once() {
     return ((double) rand() / RAND_MAX) * 2.0 - 1.0; //between -1.0 and 1.0
 }
 
-double* create_linear_model(int input_dim) {
+double *create_linear_model(int input_dim) {
     double *weights;
     weights = malloc((input_dim + 1) * sizeof(double));
     srand(time(NULL)); //seed
@@ -22,15 +22,35 @@ double* create_linear_model(int input_dim) {
     return weights;
 }
 
-double predict_linear_model_regression(double * model, double * imputs){
+
+double *train_rosenblatt_linear_model(double *model, double *dataset_inputs, double *dataset_expected_outputs,
+                                      int iterations_count = 10, float alpha = 0.01) {
+    int input_size = sizeof(model) - 1;
+    int sample_count = sizeof(dataset_inputs) / input_size;
+
+    srand(time(NULL));
+    for (int i = 0; i < iterations_count; i++) {
+        int k = rand() % sample_count;
+        double Xk[input_size];
+        int temp = 0;
+        for (int j = k * input_size; j < (k + 1) * input_size; ++j) {
+            Xk[temp] = dataset_inputs[j];
+            temp++;
+        }
+        double Yk = dataset_expected_outputs[k * 1];
+    }
+}
+
+
+double predict_linear_model_regression(double *model, double *imputs) {
     double sum_rslt = model[0];
     for (int i = 1; i < sizeof(model); ++i) {
-        sum_rslt += model[i] * imputs[i-1];
+        sum_rslt += model[i] * imputs[i - 1];
     }
     return sum_rslt;
 }
 
-short predict_linear_classification(double * model, double * imputs) {
+short predict_linear_classification(double *model, double *imputs) {
     double pred = predict_linear_model_regression(model, imputs);
     //double rslt;
     //if(pred >= 0){return 1.0;}else{return -1.0;}
--- lib/linear_model.h
+++ lib/linear_model.h
@@ -10,5 +10,5 @@ extern LIB_LINEAR_MODEL_H short predict_linear_classification(double *, double *
 extern LIB_LINEAR_MODEL_H double predict_linear_model_regression(double *, double *);
 extern LIB_LINEAR_MODEL_H double* train_linear_classification(double *, double *, double*, int , int );
 extern LIB_LINEAR_MODEL_H double get_random_once();
-
+extern LIB_LINEAR_MODEL_H double* train_rosenblatt_linear_model(double *, double *, double *, int , float);
 #endif //LIB_LINEAR_MODEL_H
