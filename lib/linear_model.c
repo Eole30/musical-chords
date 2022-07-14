ec93b80ee2f662bb7e277d9b8f5bc5ba73025a17
Elisabeth <e.fauquet3@gmail.com>
2022-07-14 13:13:19 +0200

    test create_linear

--- lib/linear_model.c
+++ lib/linear_model.c
@@ -9,19 +9,20 @@
 
 double get_random() {
     srand(time(NULL)); //seed
-    return ((double)rand() / RAND_MAX) * 2.0 - 1.0; //between -1.0 and 1.0
+    return ((double) rand() / RAND_MAX) * 2.0 - 1.0; //between -1.0 and 1.0
 }
 
-double* create_linear_model(int imput_dim){
-    double * weights = NULL;
-    weights = malloc((imput_dim+1) * sizeof(double ));
-    for(int i=0, i<imput_dim+1,i++){
-        weights[i]= get_random();
+double* create_linear_model(int input_dim) {
+    double *weights;
+    weights = malloc((input_dim + 1) * sizeof(double));
+    for (int i = 0; i < (input_dim + 1); i++) {
+        weights[i] = get_random();
     }
-    return weights
+    return weights;
 }
 
-short predict_linear_classification(double * weights, double * inputs, int size){
+
+short predict_linear_classification(double *weights, double *inputs, int size) {
     double total_sum;
     total_sum = *weights;
     for (int i = 0; i < size; ++i) {
@@ -30,17 +31,17 @@ short predict_linear_classification(double * weights, double * inputs, int size)
     return (total_sum >= 0) ? 1 : -1;
 }
 
-double* train_linear_classification(double * weights, double * inputs, double * outputs, int size, int train_iterations){
+double *train_linear_classification(double *weights, double *inputs, double *outputs, int size, int train_iterations) {
     int k;
     double g_x_k, y_k, diff;
-    for (int i = 0; i < train_iterations ; ++i) {
+    for (int i = 0; i < train_iterations; ++i) {
         srand(time(NULL)); //seed
         k = rand() % size; //get random index between 0 and len(X)
         g_x_k = predict_linear_classification(weights, inputs, size);
         y_k = *(outputs + k);
         diff = y_k - g_x_k;
         *weights += 0.01 * diff;
-        for (int j = 1; j < size ; ++j) {
+        for (int j = 1; j < size; ++j) {
             *(weights + j) += 0.01 * diff * *(inputs + j);
         }
     }
--- lib/linear_model.h
+++ lib/linear_model.h
@@ -5,8 +5,8 @@
 #ifndef LIB_LINEAR_MODEL_H
 #define LIB_LINEAR_MODEL_H
 
-short predict_linear_classification(double *, double *, int );
-double* train_linear_classification(double *, double *, double*, int , int );
-double get_random();
-double* create_linear_model(int);
+extern LIB_LINEAR_MODEL_H short predict_linear_classification(double *, double *, int );
+extern LIB_LINEAR_MODEL_H double* train_linear_classification(double *, double *, double*, int , int );
+extern LIB_LINEAR_MODEL_H double get_random();
+extern LIB_LINEAR_MODEL_H double* create_linear_model(int);
 #endif //LIB_LINEAR_MODEL_H
